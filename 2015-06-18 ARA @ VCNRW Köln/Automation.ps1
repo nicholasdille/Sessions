778f3d62e30ac1d6afa867cf71de605b9d504af3
Nicholas Dille <nicholas.dille@mailbox.org>
2015-06-18 14:00:01 +0200

    Demo von ARA-PowerShell für die VCNRW

---
+++ 2015-06-18 ARA @ VCNRW Köln/Automation.ps1
@@ -0,0 +1,17 @@
+﻿# Import credentials
+Import-AzurePublishSettingsFile -PublishSettingsFile "$Env:UserProfile\Documents\TEMP\MVP-6-17-2015-credentials.publishsettings"
+
+# Get and filter ARA collections
+Get-AzureRemoteAppCollection | select Name,Plan,Region,Type,Status | Format-Table -AutoSize
+
+# List ARA cmdlets
+Get-Command Get-AzureRemoteApp*
+
+# Available plans
+Get-AzureRemoteAppPlan | select Name,CoresPerUser,MinimumBilledUserCount
+
+# List RemoteApps
+Get-AzureRemoteAppProgram -CollectionName saashybrid | select Name,Status
+
+# Session management
+Get-Command *-AzureRemoteAppSession
