12e801f048f2b978524e93b0a73ff2adb55388a2
Nicholas Dille <nicholas.dille@mailbox.org>
2019-07-31 23:06:19 +0200

    Updates

---
+++ containers/docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit
+
+if test -z "${SOURCE}"; then
+    echo You must set SOURCE to a git URL. Commiting suicide.
+    exit 1
+fi
+if test -z "${ROOT}"; then
+    ROOT=/
+fi
+
+git clone "${SOURCE}" /tmp/content
+
+exec nginx -g "daemon off;"
