f9c2b383fac01bf66ce18ed0991024eef9a1667b
Nicholas Dille <nicholas.dille@mailbox.org>
2019-11-11 15:01:39 +0100

    Demos on in appropriate directories

--- containers/demo.sh
+++ containers/demo.sh
@@ -32,7 +32,7 @@ echo -e "${YELLOW}    Done.${DEFAULT}"
 INCLUDES=$(xmlstarlet sel -N x="http://www.w3.org/1999/xhtml" -t -m "//x:section/@data-markdown" -v . -n "${FILE}" | grep -vE '^$')
 DIRS=$(for INCLUDE in ${INCLUDES}; do echo $(dirname ${INCLUDE}); done)
 
-#DIRS=$(echo "${DIRS}" | while read DIR; do if [[ "$(ls ${DIR}/*.demo 2>/dev/null)" != "" ]]; then echo ${DIR}; fi; done)
+DIRS=$(echo "${DIRS}" | while read DIR; do if [[ "$(ls ${DIR}/*.demo 2>/dev/null)" != "" ]]; then echo ${DIR}; fi; done)
 
 for DIR in ${DIRS}; do
     if [[ "$(ls ${DIR}/*.demo 2>/dev/null)" == "" ]]; then
