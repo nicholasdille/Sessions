d8446b4626871a6bc1d45187e4ef46caf5fb7a70
Nicholas Dille <nicholas.dille@mailbox.org>
2019-11-12 23:33:49 +0100

    Fixes

--- containers/110_ecosystem/trivy/prep.sh
+++ containers/110_ecosystem/trivy/prep.sh
@@ -3,4 +3,6 @@
 curl -sLf https://github.com/aquasecurity/trivy/releases/download/v0.1.7/trivy_0.1.7_Linux-64bit.tar.gz | tar -xvz -C /usr/local/bin trivy
 
 # Download databases to avoid delay
-trivy --refresh
+if [[ ! -d ~/.cache/trivy ]]; then
+    trivy --refresh
+fi
--- containers/demo_cleanup.sh
+++ containers/demo_cleanup.sh
@@ -6,15 +6,10 @@ echo
 echo -e "${YELLOW}Waiting for demo to end. Press enter to continue...${DEFAULT}"
 read
 
-for DIR in ${DIRS}; do
-    echo
-    echo -e "${YELLOW}### Cleaning up in ${DIR}${DEFAULT}"
-    NAME=${DIR////-}
-    NAME=${NAME//_/}
-    if hcloud server list --selector demo=true,dir=${NAME} | grep --quiet "${NAME}"; then
-        hcloud server delete ${NAME}
-    fi
+hcloud server list -l demo=true -o columns=name | tail -n +2 | while read NAME; do
+    hcloud server delete ${NAME}
 done
 
 hcloud ssh-key delete demo
 rm id_rsa_demo*
+ssh docker-hcloud rm ~/id_rsa_demo
