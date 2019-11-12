cf115eff50b1be54f8d06608a268d9b24cf84282
Nicholas Dille <nicholas.dille@mailbox.org>
2019-11-13 00:53:14 +0100

    Fixes

--- containers/110_ecosystem/cnab/prep.sh
+++ containers/110_ecosystem/cnab/prep.sh
@@ -14,8 +14,8 @@ done
 
 # cnab-to-oci
 docker build --tag cnab2oci:v0.3.0-beta1 --target build --build-arg BUILDTIME=$(date +%Y%m%d%H%M) --build-arg TAG=v0.3.0-beta1 github.com/docker/cnab-to-oci#v0.3.0-beta1
-docker rm cnab2oci
-docker create --name cnab2oci cnab2oci
-rm /usr/local/bin/cnab-to-oci
+docker ps --filter name=cnab2oci -aq | xargs -r docker rm
+docker create --name cnab2oci cnab2oci:v0.3.0-beta1
+rm -f /usr/local/bin/cnab-to-oci
 docker cp cnab2oci:/go/src/github.com/docker/cnab-to-oci/bin/cnab-to-oci /usr/local/bin/cnab-to-oci
-docker rm cnab2oci
+docker ps --filter name=cnab2oci -aq | xargs -r docker rm
